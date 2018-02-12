cd4433f08fe59ba885fbbb39a6de0498cbba5f4d
jonathan.gross <jonathan.gross@wustl.edu>
2018-02-11 23:01:04 -0600

    S3 Class System Done
    Completed Part One of HW, doing assignment with S3 class sstem

--- ps3.R
+++ ps3.R
@@ -2,4 +2,28 @@
 # Pol Sci 5625
 #PS 3
 
+# S3 Methodology
+rm(list = ls())
 
+PickDoor = structure(c(doorVal = floor(runif(1,1,4))),class="door").PickDoor
+
+setValue.Door = function(door,x){ # If want to manually change value
+  door[[1]] = x
+}
+
+setValue.Door(PickDoor,1)
+
+
+playGame = function(x){
+  UseMethod("playGame",x)
+}
+
+playGame.door = function(x){
+  pickedDoor = x[[1]]
+  CarDoor = floor(runif(1,1,4))
+  if(pickedDoor == CarDoor){
+    return("Congratulations You've Won A Car")
+  }else{
+    return("I'm Sorry You Didn't Win")
+  }
+}
