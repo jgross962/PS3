f9132f792c1ea54a496118b3098e5d3bc837d5b1
jonathan.gross <jonathan.gross@wustl.edu>
2018-02-15 02:07:12 -0600

    Activity 5 Sorter
    Part 2 of Sorter (Activity 5) done. Also added Documentation to part 1

--- Activity5.R
+++ Activity5.R
@@ -1,7 +1,14 @@
-#Activity 5
+# Jonathan Gross
+# 435133
+# Pol Sci 5625
+# Activity 5
+rm(list = ls())
 
-#Create Student
+#Create Student Function 
+#Input: @name = string to name student
+#Output: object of class student that has a name and 4 integer values for name courage intellegence and ambition
 createStudent = function(name){
+  # Attributes are randomly assigned from 1 -100
   courage = floor(runif(1,1,101))
   intellegence = floor(runif(1,1,101))
   ambition = floor(runif(1,1,101))
@@ -9,23 +16,22 @@ createStudent = function(name){
   return(structure(list("name" =name,"courage" = courage,"intellgence" = intellegence,"ambition" = ambition,"effort" = effort), class = "student"))
 }
 
+# Test Student: Create example student
 j = createStudent("jon")
+j
 
-#Sorter
-x <- c(1:100)
-index = x[which(x==max(x))]
-
-sort.student = function(stud, x){
+#Sorter Method for Generic Sort Function
+#Inputs: @stud = student object to be sorted
+#     @x, a 4x4 matrix correlating attributes to houses, defaults to identity matrix
+#Output: House which student is sorted into
+sort.student = function(stud, x= diag(4)){
+  # Get 4 attributes
   a = c(stud[[2]],stud[[3]],stud[[4]],stud[[5]])
+  # Matrix multiply transpose(x)*t
   vect = (t(x)%*%a)
-  firstElement = vect[1]
-  vect = sort(vect,decreasing=T)
-  index = 0
-  for (i in 1:4){
-    if (firstElement == vect[i]){
-      index = i
-    }
-  }
+  index = which.max(vect)
+
+  #Return Appropriate Sorting
   if (index == 1){
     return("GRYFFINDOR")
   }  else if (index == 2){
@@ -37,6 +43,12 @@ sort.student = function(stud, x){
   }
 }
 
-a = diag(4)
-a
-sort(j,a)
+# Test
+sort(j)
+a = diag(4) 
+sort.student(j,a)
+
+
+
+
+
